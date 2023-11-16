5ad09f64c043f18f1866acad06aec4c2d80f3295
mohammedavez125 <mohammedavez125@gmail.com>
2023-11-16 11:16:07 +0530

    Lab 4 Astar algo,3 absent

--- DFS.py
+++ DFS.py
@@ -14,31 +14,32 @@ Created on Thu Nov  2 09:49:25 2023
 from collections import defaultdict
 class Graph:
     def __init__(self):
-        self.Graph=defaultdict(list)
-    def addEdge(self,u,v):
+        self.Graph = defaultdict(list)
+        self.num_vertices = 0
+    def addEdge(self, u, v):
+        self.num_vertices = max(self.num_vertices, u, v)
         self.Graph[u].append(v)
-    def DFSUtil(self,v,visited):
-        visited[v]=True
+    def DFSUtil(self, v, visited):
+        visited[v] = True
         print(v)
         for i in self.Graph[v]:
-            if visited[i] == False:
-                self.DFSUtil(i, visited)
-    def DFS(self):
-    # Determine the maximum vertex number
-        v = max(self.Graph.keys()) + 1
-        visited = [False] * v
-        for i in range(v):
             if not visited[i]:
                 self.DFSUtil(i, visited)
-g=Graph()
-g.addEdge(0,1)
-g.addEdge(0,2)
-g.addEdge(2,3)
-g.addEdge(3,4)
-g.addEdge(3,5)
-g.addEdge(3,6)
-print("Following is DFS (starting from vertex 1)")
+    def DFS(self):
+        visited = [False] * (self.num_vertices + 1)  
+        for i in range(self.num_vertices + 1): 
+            if not visited[i]:
+                self.DFSUtil(i, visited)
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(2, 3)
+g.addEdge(3, 4)
+g.addEdge(3, 5)
+g.addEdge(4, 2)
+print("Following is DFS (starting from vertex 0)")
 g.DFS()
+
 """
 Following is DFS (starting from vertex 1)
 0
